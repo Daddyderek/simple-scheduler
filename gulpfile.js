337c9fe18ce05f575996cb0a297eaee771cd9c2e
daddyderek <daddyderekahn@gmail.com>
2014-09-27 23:50:47 -1000

    removing datepicker

--- gulpfile.js
+++ gulpfile.js
@@ -51,8 +51,7 @@ gulp.task('copy', function () {
     './bower_components/foundation/js/foundation.js',
     './bower_components/foundation/js/foundation/foundation.topbar.js',
     './bower_components/Calendario/js/jquery.calendario.js',
-    './bower_components/Calendario/js/modernizr.custom.63321.js',
-    './bower_components/foundation-datepicker/js/foundation-datepicker.js'
+    './bower_components/Calendario/js/modernizr.custom.63321.js'
   ]).pipe(uglify())
     .pipe(gulp.dest('./public/js/libs'));
 
