eb00338a01cfebadc5f128c35f70a4f00963be12
daddyderek <daddyderekahn@gmail.com>
2014-09-28 22:01:10 -1000

    adding an add button to scheduling someone

--- public/js/xhr.js
+++ public/js/xhr.js
@@ -1,3 +1,7 @@
 $(function() {
   $(".datepicker").datepicker();
+  $('.add-worker').click(function(e) {
+    var plusOneEmployee = $('.selectEmployee').html();  
+    $('.employee-container').append(plusOneEmployee);
+  });
 });
