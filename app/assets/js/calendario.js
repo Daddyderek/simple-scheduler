a0284a7c4215a62fb920b3c1ce745c0511ca2977
daddyderek <daddyderekahn@gmail.com>
2014-09-29 15:34:11 -1000

    removing console.logs and centering inputs

--- app/assets/js/calendario.js
+++ app/assets/js/calendario.js
@@ -21,7 +21,6 @@ $(function() {
     $.getJSON('/shifts/' + year, function(json) {
       var shiftData = {};
       var element;
-      console.log(json);
       _.forEach(json, function(shift, i) {
         _.forEach(shift.employees, function(emp, i) {
           element = '<div class="' + shift.shift + '" data="' + shift.date + '">' + emp + '</div>';
@@ -31,7 +30,6 @@ $(function() {
             shiftData[shift.date] += element;
           }
         });
-        console.log(shiftData[shift.date]);
       });
       cal.setData(shiftData);
     });
