25d0d2e1cae5bf049973d5d4c1bc8d76b9175a13
daddyderek <daddyderekahn@gmail.com>
2014-10-11 00:51:15 -1000

    redirecting to calendar view after deleting shift

--- app/controllers/shifts.js
+++ app/controllers/shifts.js
@@ -150,6 +150,6 @@ module.exports.deleteShift = function(req, res) {
   }, function(err, shift) {
     if (err) throw err;
     console.log('Deleted shift', shift);
-    res.redirect('/admin');
+    res.redirect('/calendar');
   });
 };
